26a1773fe2366fa129d63a39aa0ffedb4ab1b28b
Hackerbk <bhairam.deen55@gmail.com>
2026-04-30 16:14:18 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,8 @@
+# handlers/__init__.py
+from .start import start_router
+from .menu import menu_router
+from .quiz import quiz_router
+from .dpp import dpp_router
+from .photo_test import photo_test_router
+from .callbacks import callback_router
+from .admin import admin_router
