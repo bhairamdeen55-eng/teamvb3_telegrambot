25bbc34747410354a60405160ef923d8f5543cde
Hackerbk <bhairam.deen55@gmail.com>
2026-04-30 16:15:29 +0530

    Create __init__.py

---
+++ services/__init__.py
@@ -0,0 +1,7 @@
+# services/__init__.py
+from .ai_service import ai_service, AIService
+from .parser_service import ParserService
+from .test_service import TestService
+from .image_service import ImageService
+from .score_service import ScoreService
+from .subscription import SubscriptionService
